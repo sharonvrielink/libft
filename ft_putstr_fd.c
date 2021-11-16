6966c254b57d1ccdae402b7fc2635653be9c68ad
Sharon Vrielink <svrielin@f1r2s7.codam.nl>
2021-11-16 13:30:21 +0100

    ft_putstr with libft.h included

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,11 +6,12 @@
 /*   By: svrielin <svrielin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/26 15:03:04 by sharonvriel   #+#    #+#                 */
-/*   Updated: 2021/11/16 13:19:10 by svrielin      ########   odam.nl         */
+/*   Updated: 2021/11/16 13:28:48 by svrielin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "libft.h"
 
 void	ft_putstr_fd(char *s, int fd)
 {
