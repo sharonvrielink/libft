2717bdfa9d912e87cd1d0968e2c12d3a77fc2093
Sharon@Codam <sharonvrielink@gmail.com>
2022-02-03 17:34:43 +0100

    removed headers from ft_numlen

--- src/ft_numlen.c
+++ src/ft_numlen.c
@@ -6,12 +6,9 @@
 /*   By: svrielin <svrielin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/03 17:15:53 by svrielin      #+#    #+#                 */
-/*   Updated: 2022/02/03 17:21:15 by svrielin      ########   odam.nl         */
+/*   Updated: 2022/02/03 17:34:14 by svrielin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "ft_printf.h"
-#include "libft/libft.h"
 
 int numlen(int n)
 {
