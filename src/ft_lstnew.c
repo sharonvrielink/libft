650b34600f48e14b712841486e4366a33c04a224
Sharon@Codam <sharonvrielink@gmail.com>
2022-09-04 14:20:11 +0200

    New function ft_lstnew

---
+++ src/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstnew.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: svrielin <svrielin@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/09/04 14:16:57 by svrielin      #+#    #+#                 */
+/*   Updated: 2022/09/04 14:19:32 by svrielin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
