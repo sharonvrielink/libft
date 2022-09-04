2fce8daa730fa4812614a2b9e65fab94bd39fa1a
Sharon@Codam <sharonvrielink@gmail.com>
2022-09-04 14:09:34 +0200

    Bonus list functions to header file

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   libft.h                                            :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: svrielin <svrielin@student.codam.nl>         +#+                     */
+/*   By: svrielin <svrielin@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/12 17:34:12 by svrielin      #+#    #+#                 */
-/*   Updated: 2022/04/14 14:33:40 by svrielin      ########   odam.nl         */
+/*   Updated: 2022/09/04 14:08:11 by svrielin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,5 +54,14 @@ int		ft_numlen(int n);
 size_t	ft_unsignednumlen_base(unsigned long long n, int base);
 char	*ft_ulltoa_base(unsigned long long n, int base);
 char	*ft_strtoupper(char *str);
-
+/*lists*/
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, vid (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list *ft_lstmap(t_list, void *(*f)(void *), void (*del)(void *));
 #endif
