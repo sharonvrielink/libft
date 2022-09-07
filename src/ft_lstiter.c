35576951ed5991eb50264663965126b13c937adb
Sharon@Codam <sharonvrielink@gmail.com>
2022-09-07 11:46:33 +0200

    Added lstiter

---
+++ src/ft_lstiter.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstiter.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: svrielin <svrielin@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/09/07 11:38:52 by svrielin      #+#    #+#                 */
+/*   Updated: 2022/09/07 11:45:17 by svrielin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while (lst && f)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
